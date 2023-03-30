fd6340ba803f5672893b6d952d0af7836768e6e3
alaa <fouzai.alaa@gmail.com>
2023-03-31 00:23:17 +0100

    fixing property

--- routes/property.js
+++ routes/property.js
@@ -101,7 +101,7 @@ router.post('/ChangeState',verifyPOSTToken,async (req,res) =>
     res.json({ status:"err",message:"problem" });
 }
 });
-router.get('/GetAll',verifyGETToken,async (req,res) =>
+router.get('/GetAlls',verifyGETToken,async (req,res) =>
 {
     let user  = await User.findOne({ _id : req.userId  });
     if (user.enabled) {
@@ -132,20 +132,33 @@ router.get('/GetAll',verifyGETToken,async (req,res) =>
                     
                 });
 
-                res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         console.log("*************************");
         res.json({status:"ok" , propertys : p});
 
             }
+        });  
+    }catch (err) {
+        res.header("Access-Control-Allow-Headers", "*");
+        res.json({ message:err.message });
+    }
+}
+});
+router.get('/GetAll',verifyGETToken,async (req,res) =>
+{
+    let user  = await User.findOne({ _id : req.userId  });
+    if (user.enabled) {
+    try{
+        let property=[];
 
-        
-        
-        
+        let f = await getPropertys(req.userId).then(async (f) => {
+            console.log(" step 1");
+        })
+        property = await getChats(f).then(async (s)=> {
+            console.log("step 2")
         });
-        //console.log(p)
-        //console.log("-----------------------------------")
-        
+       console.log("step 3")
         
     }catch (err) {
         res.header("Access-Control-Allow-Headers", "*");
@@ -153,6 +166,84 @@ router.get('/GetAll',verifyGETToken,async (req,res) =>
     }
 }
 });
-
-
+ async function getPropertys(id) {
+    let f =await Property.find({"Users.admin" : Mongoose.Types.ObjectId(id) } ).lean();
+    /* {
+            _id: 6407bdd152341d6248dfff37,
+            Created_date: 2023-03-07T22:42:14.883Z,
+            state: true,
+            Users: [ [Object] ],
+            Products: [],
+            Name: 'tchat',
+            __v: 0
+        } */
+    return new Promise(function(resolve, reject) {
+        //console.log(f);
+        resolve(f) // successfully fill promise
+    })
+}
+async function getChats(p) {
+    let property=[];
+    p.forEach((item) => {
+        //console.log(item)
+        let x ={}
+        x._id = item._id;
+        x.Created_date = item.Created_date;
+        x.state = item.state;
+        x.Users = item.Users;
+        x.Name = item.Name;
+        //console.log(item.Products.length);
+        //console.log(typeof(item.Products))
+        if ( item.Products.length > 0 ) {
+            //console.log("typeof(item.Products)")
+            item.Products.forEach(async (p) => {
+                if (p.hasOwnProperty('chat')){
+                    let c = await getChat(p.chat)
+                    x.Products = [];
+                    x.Products.push(c);
+                }
+            })
+        }else{
+            x.Products = [];
+        }
+        property.push(x);
+    });
+    return new Promise(function(resolve, reject) {
+        //console.log(f);
+        resolve(property) // successfully fill promise
+    })
+}
+async function getChat(p) {
+    let c = await Chat.findOne({ _id : Mongoose.Types.ObjectId(p) });
+                    console.log(c);
+                    let pr = {}
+                    
+                    pr.Created_date = c.Created_date;
+                    pr.state = c.state;
+                    pr.chatBot = c.chatBot;
+                    pr.Users = c.Users;
+                    pr._id = c._id;
+                    pr.Name = c.Name;
+                    pr.Style = c.Style;
+                    pr.Script = c.Script;
+    return new Promise(function(resolve, reject) {
+        console.log("pr");
+        console.log(pr);
+        resolve(pr) // successfully fill promise
+    })
+}
+/*
+                    {
+                        Created_date: 2023-03-07T22:05:04.129Z,
+                        state: true,
+                        chatBot: [],
+                        messages: [],
+                        Users: [ { admin: 63f9fd6ec58ecd3304639e83 } ],
+                        _id: 6407b51877ee6e67b47a83d8,
+                        Name: 'azeaze',
+                        Style: 'files/chatstyle.css',
+                        Script: 'chatScript.js',
+                        Property: '64039b84dc9a8636105bc86f',
+                        __v: 0
+                        }*/
 module.exports = router;
